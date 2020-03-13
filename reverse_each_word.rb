28329a1a00fc2e516745913320c3900d7af5e829
Charles Butler <cdbutle@outlook.com>
2020-03-13 02:16:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-def reverse_each_word(array)
-  array = []
-  array.each do |word|
-    array<< "#{array.reverse}?"
+def reverse_each_word(string)
+  sentence_array = sentence.split(" ")
+  answer = []
+  
+  answer = sentence_array.collect (|word| word.reverse}
+  answer.join(" ")
   end
 end
