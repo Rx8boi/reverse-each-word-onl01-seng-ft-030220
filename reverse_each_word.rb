a7352dea3d35d449a1dd2d04b26c9e352ebcc3f3
Charles Butler <cdbutle@outlook.com>
2020-03-13 02:19:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(string)
   sentence_array = string.split(" ")
   new_array = []
   new_array = sentence_array.collect {|word| word.reverse}
-  answer.join(" ")
+  new_array.join(" ")
 end
